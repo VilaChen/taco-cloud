ec075f9302e63776c5a13a2d38c61aaf9a09d25c
陈 <112319411+VilaChen@users.noreply.github.com>
2024-03-26 22:36:24 +0800

    update junit package path

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -1,6 +1,6 @@
 package tacos;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
