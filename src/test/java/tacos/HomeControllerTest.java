c49153f65f31b2dd5e90c682bf08f36a98f4e507
陈 <112319411+VilaChen@users.noreply.github.com>
2024-04-04 00:35:32 +0800

    fix test error

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -1,14 +1,16 @@
 package tacos;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+@RunWith(SpringRunner.class)
 @WebMvcTest
 public class HomeControllerTest {
     @Autowired
@@ -18,7 +20,7 @@ public class HomeControllerTest {
     public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("Home"))
+                .andExpect(view().name("home"))
                 .andExpect(content().string(containsString("Welcome to ...")));
     }
 }
